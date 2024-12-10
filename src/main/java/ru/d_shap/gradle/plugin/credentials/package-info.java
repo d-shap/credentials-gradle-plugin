cbfc97cc1e20b0db762d65f8125e9a05a988ccb2
Dmitry Shapovalov <shenf@mail.ru>
2024-12-10 18:04:45 +0300

    Добавил javadoc для пакета

---
+++ src/main/java/ru/d_shap/gradle/plugin/credentials/package-info.java
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Credentials Gradle Plugin is a plugin to obtain JKS keystore credentials.
+// Copyright (C) 2024 Dmitry Shapovalov.
+//
+// This file is part of Credentials Gradle Plugin.
+//
+// Credentials Gradle Plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Credentials Gradle Plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Credentials Gradle Plugin is a plugin to obtain JKS keystore credentials.
+ */
+package ru.d_shap.gradle.plugin.credentials;
